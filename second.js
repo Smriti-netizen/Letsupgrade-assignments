98e11e322f0851d46de1bc04d59dbde9c2525f62
Smriti-netizen <76514589+Smriti-netizen@users.noreply.github.com>
2021-01-27 16:44:16 +0530

    Second Assignment Letsupgrade

---
+++ second.js
@@ -0,0 +1,12 @@
+//Question 1
+const clothes=['jacket','t-shirt'];
+clothes.length=0;
+console.log(clothes[0])
+//Question 2
+var arr=[5,6,9,3,8];
+num=arr.length;
+var sum=0;
+for(var i=0;i<num;i++){
+    sum=sum+arr[i]
+}
+console.log(sum)
